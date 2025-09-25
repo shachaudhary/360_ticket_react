8dee4567e83c9f15e6dd984c8fe8e56dd7e9b87b
amanali033 <amanameerali.03@gmail.com>
2025-09-26 00:41:32 +0500

    Code Push

--- src/config/api/apiAuth.js
+++ src/config/api/apiAuth.js
@@ -4,7 +4,7 @@ export const createAPIEndPointAuth = (endpoint) => {
   const BASE_URL = import.meta.env.VITE_APP_BASE_URL_AUTH;
   const X_API_Key = import.meta.env.VITE_APP_X_API_Key_AUTH;
 
-  console.log("VITE_APP_X_API_Key_AUTH", import.meta.env.VITE_APP_X_API_Key_AUTH);
+  console.log("VITE_APP_X_API_Key_AUTH", import.meta.env.VITE_APP_X_API_Key_AUTH); 
 
   let token = typeof localStorage !== 'undefined' && localStorage.getItem('access_token');
 
