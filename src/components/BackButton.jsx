09a62e36a4df7adcc6d33853b5a29a2a086734e2
amanali033 <amanameerali.03@gmail.com>
2025-10-11 01:18:41 +0500

    theme

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -28,7 +28,7 @@ const BackButton = ({ self = null }) => {
         backgroundColor: "primary.main", // Light grey background
         borderRadius: "24px",
         "&:hover": {
-          backgroundColor: "#824EF2",
+          backgroundColor: "secondary.main",
           color: "primary.main",
         },
       }}
