8c4ebe3337aa86945b759584a5a75017a5cd9f4d
amanali033 <amanameerali.03@gmail.com>
2025-12-15 19:46:59 +0500

    loader for stats

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -158,7 +158,7 @@ export default function Dashboard() {
 
   return (
     <div className="dashbaord-pg">
-      {loadingStats && timeView === "custom" && (
+      {loadingStats && (
         <div className="h-[calc(100vh-56px)] absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 rounded-md !z-50 backdrop-blur-sm">
           <CircularProgress size={40} thickness={4} sx={{ color: "#9C6BFF" }} />
         </div>
